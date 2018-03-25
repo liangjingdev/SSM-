0f13b2b32a96f1e558fb3b70169ca8509c2525cd
梁竞 <1184106223@qq.com>
2018-03-25 16:19:49 +0800

    单元测试基类、

---
+++ src/test/java/cn/jing/canpusShop/BaseTest.java
@@ -0,0 +1,19 @@
+package cn.jing.canpusShop;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * function:单元测试基类，用来配置Spring和junit整合，junit启动时加载Spring IOC容器。
+ * ／／@RunWith注解用来告诉junit，spring是用哪个类来跑单元测试的。（spring与junit整合）
+ * 
+ * @author liangjing
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉junit，spring配置文件的位置，则将会被自动载入
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
+public class BaseTest {
+
+}
