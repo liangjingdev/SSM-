a141cc23244b4f13a4cb80cc16c97c293927b9b2
梁竞 <1184106223@qq.com>
2018-04-02 23:07:32 +0800

    平台账号实体类、

--- src/main/java/cn/jing/campusShop/entity/LocalAuth.java
+++ src/main/java/cn/jing/campusShop/entity/LocalAuth.java
@@ -4,52 +4,69 @@ import java.util.Date;
 
 /**
  * function:本地账号实体类
+ * 
  * @author liangjing
  *
  */
 public class LocalAuth {
-	
+
+	// 主键Id
 	private Long localAuthId;
+	// 账号
 	private String username;
+	// 密码
 	private String password;
+	// 创建时间
 	private Date createTime;
+	// 最近一次的更新时间
 	private Date lastEditTime;
+	// 个人信息，关系一一对应
 	private PersonInfo personInfo;
-	
-	
+
 	public Long getLocalAuthId() {
 		return localAuthId;
 	}
+
 	public void setLocalAuthId(Long localAuthId) {
 		this.localAuthId = localAuthId;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Date getCreateTime() {
 		return createTime;
 	}
+
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
+
 	public Date getLastEditTime() {
 		return lastEditTime;
 	}
+
 	public void setLastEditTime(Date lastEditTime) {
 		this.lastEditTime = lastEditTime;
 	}
+
 	public PersonInfo getPersonInfo() {
 		return personInfo;
 	}
+
 	public void setPersonInfo(PersonInfo personInfo) {
 		this.personInfo = personInfo;
 	}
