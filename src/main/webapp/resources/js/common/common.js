a21e03525f4e76a338e0e2d8a865495d592e0577
梁竞 <1184106223@qq.com>
2018-04-28 20:04:29 +0800

    辅助型js文件、

---
+++ src/main/webapp/resources/js/common/common.js
@@ -0,0 +1,41 @@
+Date.prototype.Format = function(fmt) {
+	var o = {
+		"M+" : this.getMonth() + 1, // 月份
+		"d+" : this.getDate(), // 日
+		"h+" : this.getHours(), // 小时
+		"m+" : this.getMinutes(), // 分
+		"s+" : this.getSeconds(), // 秒
+		"q+" : Math.floor((this.getMonth() + 3) / 3), // 季度
+		"S" : this.getMilliseconds()
+	// 毫秒
+	};
+	if (/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
+			.substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
+				: (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
+}
+
+/**
+ * function：点击进行更换验证码
+ */
+//# sourceURL=dynamicScript.js 
+function changeVerifyCode(img) {
+	img.src = "../Kaptcha?" + Math.floor(Math.random() * 100);
+}
+
+/**
+ * function:根据url传递过来的key去获取它的值(如shopId)
+ * （比如url：http://localhost:8080/campusShop/shopadmin/getshopbyid?shopId=1，只要传入的key是shopId，那么就能获取到它的值--getQueryString(shopId)）
+ */
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) {
+		return decodeURIComponent(r[2]);
+	}
+	return '';
+}
