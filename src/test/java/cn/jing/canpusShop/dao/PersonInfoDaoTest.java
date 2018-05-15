7697547c0f5861f026c20da77e8b2fddae28887b
梁竞 <1184106223@qq.com>
2018-05-15 09:52:49 +0800

    Dao层测试文件、

---
+++ src/test/java/cn/jing/canpusShop/dao/PersonInfoDaoTest.java
@@ -0,0 +1,42 @@
+package cn.jing.canpusShop.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.jing.campusShop.dao.PersonInfoDao;
+import cn.jing.campusShop.entity.PersonInfo;
+import cn.jing.canpusShop.BaseTest;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PersonInfoDaoTest extends BaseTest {
+	@Autowired
+	private PersonInfoDao personInfoDao;
+
+	@Test
+	public void testAInsertPersonInfo() throws Exception {
+		PersonInfo personInfo = new PersonInfo();
+		personInfo.setName("我爱你");
+		personInfo.setGender("女");
+		personInfo.setUserType(1);
+		personInfo.setCreateTime(new Date());
+		personInfo.setLastEditTime(new Date());
+		personInfo.setEnableStatus(1);
+		int effectedNum = personInfoDao.insertPersonInfo(personInfo);
+		assertEquals(1, effectedNum);
+	}
+
+	@Test
+	public void testBQueryPersonInfoById() {
+		long userId = 1;
+		PersonInfo personInfo = personInfoDao.queryPersonInfoById(userId);
+		System.out.println(personInfo.getName());
+	}
+
+}
